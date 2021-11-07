874ef746394c9a8cea7fcf82b06aa95bdff48c24
Roxana <nolvia.roxana.aguilera@gmail.com>
2021-11-07 20:14:39 +0100

    Añade pruebas de documentación

--- ejercicio_4_1/src/ejercicio_4_1/Segunda.java
+++ ejercicio_4_1/src/ejercicio_4_1/Segunda.java
@@ -11,6 +11,8 @@ public class Segunda {
 
 	/**
 	 * @param args
+	 * @author <h1>Roxana</h1>
+	 * @see <p>Ver aquí</sp>
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
