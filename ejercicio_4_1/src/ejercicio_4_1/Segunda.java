1d057e794ea70bc85efaf3166f482f0155478159
Roxana <nolvia.roxana.aguilera@gmail.com>
2021-11-07 16:59:32 +0100

    Añade segunda clase

---
+++ ejercicio_4_1/src/ejercicio_4_1/Segunda.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejercicio_4_1;
+
+/**
+ * @author nolvi
+ *
+ */
+public class Segunda {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
