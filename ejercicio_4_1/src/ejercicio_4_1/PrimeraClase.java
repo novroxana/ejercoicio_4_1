01171a898315898addc5acfc1b55cec11308f598
Roxana <nolvia.roxana.aguilera@gmail.com>
2021-11-07 16:58:30 +0100

    Añade Primera clase

---
+++ ejercicio_4_1/src/ejercicio_4_1/PrimeraClase.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ejercicio_4_1;
+
+/**
+ * @author nolvi
+ *
+ */
+public class PrimeraClase {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
